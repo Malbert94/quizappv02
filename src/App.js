ae9c7cbf71b430e2709f898cb77770b0cc4e10a1
ALBERT Martin <martin.albert@soprasteria.com>
2019-11-26 09:23:44 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { quizBook } from './QuizBook';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      score: 0,
+      maxScore: 0,
+      subject: '',
+      quizBook: [],
+      activeQuiz: {},
+      correctAnswers: [],
+      imagelink: '',
+      textInput: '',
+      userAnswers: [],
+      useStyle: false
+    }
+
+    this.checkAnswers = this.checkAnswers.bind(this)
+  }
+
+
+  handleSelectBook(selectedBook) {
+    this.setState({
+      activeQuiz: selectedBook,
+      subject: selectedBook.Tema,
+      maxScore: selectedBook.answers.length,
+      correctAnswers: selectedBook.answers,
+      imagelink: selectedBook.image,
+      useStyle: false,
+      textInput: '',
+      userAnswers: [''],
+      score: 0
+    })
+  }
+
+  handleTextInput(index, event) {
+    var userAnswers = this.state.userAnswers
+    userAnswers[index] = event.target.value
+    this.setState({
+      textInput: event.target.value,
+      userAnswers: userAnswers
+    })
+    console.log(this.state.userAnswers, index)
+  }
+
+  checkAnswers() {
+    var answers = this.state.userAnswers
+    var correct = this.state.correctAnswers
+    var points = 0
+
+    for(let i=0; i < answers.length; i++) {
+      if(answers[i] === correct[i]) {
+        points = points + 1
+        console.log('Correct' + i);
+      }
+    }
+    this.setState({score: points, useStyle: true})
+    console.log(points);
+  }
+
+  componentDidMount() {
+    this.setState({quizBook: quizBook})
+  }
+
+  render() {
+    return(
+      <div className='app-main'>
+        <div className='header'>
+          <h1>Quiz</h1>
+          <div className='button-list'>
+            {this.state.quizBook.map((tema, index) =>
+              <button className='subjectButton' key={index} onClick={this.handleSelectBook.bind(this, quizBook[index])}>
+                {quizBook[index].Tema}
+              </button>
+            )}
+          </div>
+          <p>Subject: {this.state.subject}</p>
+          <p>Points: {this.state.score}/{this.state.maxScore}</p>
+        </div>
+        <button className='answerButton' onClick={this.checkAnswers}>
+          Check Answers
+        </button>
+        <div className='quiz-container'>
+          <ol className='input-list'>
+
+            {this.state.correctAnswers.map((answer, index) =>{
+
+              if(this.state.useStyle === false) {
+                return(
+                  <li key={index}>
+                    <input
+                      value={this.state.userAnswers[index] || ''}
+                      onChange={this.handleTextInput.bind(this, index)}
+                      className='user-input'
+                    />
+                  </li>)
+              } else {
+                if(answer === this.state.userAnswers[index]) {
+                  return(
+                    <li key={index}>
+                      <input
+                        value={this.state.userAnswers[index] || ''}
+                        disabled={true}
+                        style={{backgroundColor: 'lightgreen'}}
+                        onChange={this.handleTextInput.bind(this, index)}
+                        className='user-input'
+                      />
+                    </li>)
+                } else {
+                  return(
+                    <li key={index}>
+                      <input
+                        value={this.state.userAnswers[index]  || ''}
+                        disabled={true}
+                        style={{backgroundColor: 'red'}}
+                        onChange={this.handleTextInput.bind(this, index)}
+                        className='user-input'
+                      />
+                    </li>)
+                }
+              }
+            })}
+          </ol>
+          {this.state.imagelink ?(
+            <img className='quiz-image' alt='Europa' src={this.state.imagelink}/>
+          ) : (
+            <div>Bildet ikke funnet</div>
+          )}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/QuizBook.js
@@ -0,0 +1,21 @@
+export const quizBook = [
+  {
+    Tema: 'European countries',
+    answers: ['Iceland', 'Norway', 'Sweden', 'Finland', 'Russia', 'Estonia',
+    'Latvia', 'Lithuania', 'Belarus', 'Ukraine', 'Georgia', 'Moldova', 'Poland',
+    'Slovakia', 'Hungary', 'Romania', 'Bulgaria', 'Greece', 'North-Macedonia',
+    'Albania', 'Serbia', 'Bosnia', 'Croatia', 'Slovenia', 'Austria', 'Czechia',
+    'Germany', 'Denmark', 'Netherland', 'Belgium', 'Luxembourg', 'Liechtenstein',
+    'Switzerland', 'Italy', 'San Marino', 'The Vatican', 'Monaco', 'France',
+    'Andorra', 'Spain', 'Portugal', 'United Kingdom', 'Ireland', 'Malta',
+    'Cyprus', 'Turkey', 'Armenia', 'Azerbaijan'],
+    image: 'https://i.pinimg.com/736x/00/62/0a/00620a35f928895cc4fa53a721c9204b--geography-activities-geography-lessons.jpg'
+  },
+  {
+    Tema: 'Asian countries',
+    answers: ['Turkey', 'Lebanon', 'Israel', 'Jordan', 'Saudi Arabia', 'Kuwait',
+    'United Arab Emirates', 'Qatar', 'Yemen', 'Oman', 'Syria', 'Iraq',
+    'Hong Kong', 'Macau'],
+    image: 'https://i.pinimg.com/736x/1d/1e/49/1d1e492fb029aede0b92e929227bf297--countries-of-asia-map-of-asia.jpg'
+  }
+]
